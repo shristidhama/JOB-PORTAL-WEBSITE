1f7171ab1675242acb0b82ba740e252aa8e4a623
Shristi <shristidhama921@gmail.com>
2024-05-18 15:43:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+/*(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>150){
+            $(".navbar").css("background","#222")
+            $(".navbar").css("box-shadow","rgba(0,0,0,0.1) 0px 4px 12px");
+        }
+        else{
+            $(".navbar").css("background","transparent");
+            $(".navbar").css("box-shadow","none");
+        }
+    })
+
+});
+
+//smooth scroll
+var navbarHeight = $(".navbar").outerHeight();
+$(".navbar-menu a").click(function(e){
+    var targetHref=$(this).atrr("href");
+    $(html,body).animate({
+        scrollTop:$(targetHref).offset().top-navbarHeight
+    },1000)
+    e.preventDefault();
+});*/
